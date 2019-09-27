e2f99c9f84b57338b4a7b72aa4fb8cc1ed729c9e
Keypad <Keypad@192.168.43.204>
2019-09-27 17:07:13 +0900

    -- daily sunset commit

--- src/kosui/ppplocalui/EcConst.java
+++ src/kosui/ppplocalui/EcConst.java
@@ -308,7 +308,7 @@ public final class EcConst {
    * @return the MARK
    */
   public static final String ccGetLastLeavingStamp(){
-    return "_1909271139";
+    return "_1909271604";
   }//+++
   
 }//***eof
